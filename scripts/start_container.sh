f9e4a12e6d399379e5255925beac8199e2a5b5cb
udaydudde143 <138751078+udaydudde143@users.noreply.github.com>
2025-12-29 14:19:54 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull udaydudde143/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 udaydudde143/simple-python-flask-app
